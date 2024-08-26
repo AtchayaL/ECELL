cdd63212c51d7872e6a625c885433c80206b8381
AtchayaL <atchayalekshmanan@gmail.com>
2024-08-26 17:23:29 +0530

    initial commit

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,16 @@
+import "./about.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <h1>About Us</h1>
+      <p>We are an online book store,Enjoy Reading!
+      </p>
+      <p>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </section>
+  );
+};
+
+export default About;
